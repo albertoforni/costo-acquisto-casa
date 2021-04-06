c7c66ae9404c9f82805a19ea5363a68cfddf33b3
Alberto Forni <me@albertoforni.com>
2021-04-06 14:52:56 +0200

    add AgencyFee

--- src/store/rules.spec.tsx
+++ src/store/rules.spec.tsx
@@ -1,10 +1,52 @@
-import { agencyFeePercentage } from "@app/store/rules";
+import { agencyFee } from "@app/store/rules";
 
-describe("Agency Fee Percentage", () => {
-  it("it returns percentage + VAT", () => {
-    expect(agencyFeePercentage({ price: 100, percentage: 3 })).toBe(3.66);
+describe("Agency Fee", () => {
+  it("returns percentage + VAT if isViaAgency is true", () => {
+    expect(
+      agencyFee({
+        kind: "PERCENTAGE",
+        price: 100,
+        percentage: 3,
+      }),
+    ).toBe(3.66);
   });
-  it("it returns 0 when % fee is 0", () => {
-    expect(agencyFeePercentage({ price: 100, percentage: 0 })).toBe(0);
+  it("returns 0 when % fee is 0", () => {
+    expect(
+      agencyFee({
+        kind: "PERCENTAGE",
+        price: 100,
+        percentage: 0,
+      }),
+    ).toBe(0);
+  });
+  it("returns 0 if AgencyFee is NONE", () => {
+    expect(
+      agencyFee({
+        kind: "NONE",
+      }),
+    ).toBe(0);
+  });
+  it("returns the value when AgencyFee is VALUE", () => {
+    expect(
+      agencyFee({
+        kind: "VALUE",
+        fee: 321,
+      }),
+    ).toBe(321);
+  });
+  it("doesn't return negative values", () => {
+    expect(
+      agencyFee({
+        kind: "VALUE",
+        fee: -321,
+      }),
+    ).toBe(0);
+    expect(
+      agencyFee({
+        kind: "PERCENTAGE",
+        price: -100,
+        percentage: 3,
+      }),
+    ).toBe(0);
   });
 });
--- src/store/rules.ts
+++ src/store/rules.ts
@@ -7,3 +7,35 @@ export function agencyFeePercentage({
 }): number {
   return price * (percentage / 100) * 1.22;
 }
+
+type AgencyFee =
+  | {
+      kind: "PERCENTAGE";
+      price: number;
+      percentage: number;
+    }
+  | {
+      kind: "NONE";
+    }
+  | {
+      kind: "VALUE";
+      fee: number;
+    };
+
+export function agencyFee(args: AgencyFee): number {
+  let fee = 0;
+  switch (args.kind) {
+    case "PERCENTAGE":
+      const { price, percentage } = args;
+      fee = agencyFeePercentage({ price, percentage });
+      break;
+    case "NONE":
+      fee = 0;
+      break;
+    case "VALUE":
+      fee = args.fee;
+      break;
+  }
+
+  return Math.max(fee, 0);
+}
